8bea9d55cd9678396d2421185a41df0511b2e37b
Alex Lee <ahjlee712@gmail.com>
2021-06-10 10:55:04 -0400

    Completed day 6

---
+++ day6-functions/main.py
@@ -0,0 +1,12 @@
+#creating functions
+
+def new_function():
+  print("Hello, this is a function")
+
+new_function()
+
+counter = 5
+
+while counter > 0:
+  print(f"Countdown {counter}")
+  counter -= 1
